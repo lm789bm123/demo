d1ceb4899607cb826a8f6899811666f9fdf76fe9
lm789bm123 <lm789bm123@gmail.com>
2021-07-16 15:59:12 +0800

    update

--- DemoDatabase/DemoDatabase/bin/Debug/DemoDatabase.sql
+++ DemoDatabase/DemoDatabase/bin/Debug/DemoDatabase.sql
@@ -40,24 +40,6 @@ USE [$(DatabaseName)];
 
 
 GO
-PRINT N'Creating [dbo].[DemoReport]...';
-
-
-GO
-CREATE PROCEDURE [dbo].[DemoReport]
-	@fronend_id varchar(10)
-AS
-BEGIN
-	SELECT m.frontend_id, p.pool_id, SUM(bf.profit_loss) AS profit_loss
-	FROM match_info m
-	INNER JOIN pools p
-	ON p.event_id = m.event_id
-	INNER JOIN bet_financial bf
-	on bf.pool_id = p.pool_id
-	WHERE frontend_id = @fronend_id
-	GROUP BY m.frontend_id, p.pool_id
-END
-GO
 PRINT N'Update complete.';
 
 
--- DemoDatabase/DemoDatabase/report/DemoReport.sql
+++ DemoDatabase/DemoDatabase/report/DemoReport.sql
@@ -1,5 +1,5 @@
 ﻿CREATE PROCEDURE [dbo].[DemoReport]
-	@fronend_id varchar(10)
+	@bet_type TINYINT
 AS
 BEGIN
 	SELECT m.frontend_id, p.pool_id, SUM(bf.profit_loss) AS profit_loss
@@ -8,7 +8,7 @@ BEGIN
 	ON p.event_id = m.event_id
 	INNER JOIN bet_financial bf
 	on bf.pool_id = p.pool_id
-	WHERE frontend_id = @fronend_id
+	WHERE p.bet_type = @bet_type
 	GROUP BY m.frontend_id, p.pool_id
 END
 
